fb81eac288d27d7cb205021b976eb083c5b81dcd
Ross Sullivan <rosssullivan101@gmail.com>
2026-04-09 22:32:56 +0900

    clean up error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,4 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("failed to open device: {0}")]
     DeviceOpen(#[from] std::io::Error),
